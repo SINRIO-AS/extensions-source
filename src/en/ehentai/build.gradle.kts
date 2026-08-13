ba2e5a705cae4112dd28ff2c59104522c2f5f4c1
SINRIO-AS <72839154+SINRIO-AS@users.noreply.github.com>
2026-08-13 04:58:34 +0000

    Update versionCode to 2 in build.gradle.kts
    Increment versionCode from 1 to 2 in build.gradle.kts.

--- src/en/ehentai/build.gradle.kts
+++ src/en/ehentai/build.gradle.kts
@@ -1,44 +1,22 @@
 import io.github.keiyoushi.gradle.api.ContentWarning
 
-
-
 plugins {
-
     alias(kei.plugins.extension)
-
 }
-
-
 
 keiyoushi {
-
     name = "E-Hentai"
-
-    versionCode = 1
-
+    versionCode = 2
     contentWarning = ContentWarning.NSFW
-
     libVersion = "1.6"
 
-
-
     source {
-
         name = "E-Hentai"
-
         baseUrl = "https://e-hentai.org"
-
         lang = "en"
-
     }
-
-
 
     deeplink {
-
         path("/..*")
-
     }
-
 }
-
