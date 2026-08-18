c4690322399dc1ee9538a17a016bb11621c84080
SINRIO-AS <72839154+SINRIO-AS@users.noreply.github.com>
2026-08-18 21:26:14 +0000

    Bump versionCode from 5 to 6

--- src/en/ehentai/build.gradle.kts
+++ src/en/ehentai/build.gradle.kts
@@ -1,27 +1,44 @@
 import io.github.keiyoushi.gradle.api.ContentWarning
 
 
+
 plugins {
+
     alias(kei.plugins.extension)
+
 }
 
 
+
 keiyoushi {
+
     name = "E-Hentai"
-    versionCode = 5
+
+    versionCode = 6
+
     contentWarning = ContentWarning.NSFW
+
     libVersion = "1.6"
 
 
+
     source {
+
         name = "E-Hentai"
+
         baseUrl = "https://e-hentai.org"
+
         lang = "en"
+
     }
+
 
 
     deeplink {
+
         path("/..*")
+
     }
+
 }
 
