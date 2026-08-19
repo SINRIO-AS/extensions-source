a2a104fb386cd91e54d772ca745ab1840049a808
SINRIO-AS <72839154+SINRIO-AS@users.noreply.github.com>
2026-08-19 01:49:33 +0000

    Increment versionCode from 10 to 11

--- src/en/ehentai/build.gradle.kts
+++ src/en/ehentai/build.gradle.kts
@@ -1,14 +1,17 @@
 import io.github.keiyoushi.gradle.api.ContentWarning
 
+
 plugins {
     alias(kei.plugins.extension)
 }
 
+
 keiyoushi {
     name = "E-Hentai"
-    versionCode = 10
+    versionCode = 11
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
+
 
     source {
         name = "E-Hentai"
@@ -16,7 +19,9 @@ keiyoushi {
         lang = "en"
     }
 
+
     deeplink {
         path("/..*")
     }
 }
+
