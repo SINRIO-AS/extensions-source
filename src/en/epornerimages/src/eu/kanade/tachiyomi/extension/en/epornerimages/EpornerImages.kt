5f499d5026b28ffd8ab0e85c1ccd594463d5d8c4
SINRIO-AS <7.2839154e+07+SINRIO-AS@users.noreply.github.com>
2026-08-24 09:46:14 +0000

    Fix Aniyomi Eporner Images formatting

--- src/en/epornerimages/src/eu/kanade/tachiyomi/extension/en/epornerimages/EpornerImages.kt
+++ src/en/epornerimages/src/eu/kanade/tachiyomi/extension/en/epornerimages/EpornerImages.kt
@@ -10,7 +10,6 @@ import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
 import keiyoushi.annotation.Source
-import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
